a63be870b51e5906959e63028de20906bc6241c1
Heejin <monster@teamappetizer.com>
2016-08-19 21:41:20 +0900

    Initial commit

---
+++ Assets/Co/Co.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public enum CoRunnerType {
+
+	/// <summary>
+	/// It will be unloaded after scene has changed
+	/// </summary>
+	INSTANT,
+
+	/// <summary>
+	/// It will be alive forever even if scene has changed
+	/// </summary>
+	PERMANENT
+}
+
+public interface ICoRunner {
+
+	Coroutine Run (IEnumerator enumerator);
+
+	Coroutine Delay (float delay_sec, Action action);
+
+	void Stop (Coroutine coroutine);
+
+	void StopAll ();
+}
+
+public static class Co {
+	private static ICoRunner instantInstance_ = null;
+	private static ICoRunner permanentInstance_ = null;
+
+	public static ICoRunner InstantRunner {
+		get {
+			if (instantInstance_ == null) {
+				var go = new GameObject("[CoRunner.Instant]");
+				instantInstance_ = go.AddComponent<CoRunner>();
+			}
+			return instantInstance_;
+		}
+	}
+
+	public static ICoRunner PermanentRunner {
+		get {
+			if (permanentInstance_ == null) {
+				var go = new GameObject("[CoRunner.Permanent]");
+				GameObject.DontDestroyOnLoad(go);
+				permanentInstance_ = go.AddComponent<CoRunner>();
+			}
+			return permanentInstance_;
+		}
+	}
+
+	#region Shortcuts
+
+	public static Coroutine Run (IEnumerator enumerator) {
+		return InstantRunner.Run(enumerator);
+	}
+
+	public static Coroutine Delay (float delay_sec, Action action) {
+		return InstantRunner.Delay(delay_sec, action);
+	}
+
+	public static void Stop (Coroutine coroutine) {
+		InstantRunner.Stop(coroutine);
+	}
+
+	public static void StopAll () {
+		InstantRunner.StopAll();
+	}
+
+	#endregion Shortcuts
+}
+
+public sealed class CoRunner : MonoBehaviour, ICoRunner {
+
+	public Coroutine Run (IEnumerator enumerator) {
+		return StartCoroutine(enumerator);
+	}
+
+	public Coroutine Delay (float delay_sec, Action action) {
+		return Run(CoDelay(delay_sec, action));
+	}
+
+	private IEnumerator CoDelay (float delay_sec, Action action) {
+		yield return new WaitForSeconds(delay_sec);
+		if (action != null)
+			action();
+	}
+
+	public void Stop (Coroutine coroutine) {
+		if (coroutine == null)
+			return;
+
+		StopCoroutine(coroutine);
+	}
+
+	public void StopAll () {
+		StopAllCoroutines();
+	}
+}
---
+++ Assets/TestScene/TestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlainClass {
+
+	public void TestCoroutine () {
+		Co.Run(CountSeconds());
+	}
+
+	private IEnumerator CountSeconds () {
+		for (var i = 1; i <= 10; ++i) {
+			yield return new WaitForSeconds(1f);
+			Debug.Log(i + " seconds elasped");
+		}
+	}
+}
+
+public class TestScript : MonoBehaviour {
+
+	private void Start () {
+		Co.Delay(1f, () => Debug.Log("after 1 second"));
+		Co.Delay(2f, () => Debug.Log("after 2 seconds"));
+
+		var plainClass = new PlainClass();
+		plainClass.TestCoroutine();
+	}
+}
